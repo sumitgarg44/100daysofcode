7500e8f366dc397c034b4cace84940f1854320e9
Sumit Garg <sumit.garg@rackspace.com>
2025-06-16 23:31:37 +0530

    Move colors to static

--- hangman.py
+++ hangman.py
@@ -1,19 +1,8 @@
 import random
 
+from static.colors import color
 from static.arts import gameover, hangman
 from static.hangman_words import word_list
-
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
 
 chosen_word = random.choice(word_list)
 
