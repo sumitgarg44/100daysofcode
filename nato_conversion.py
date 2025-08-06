39dc6c57de4b2ebc693adf1cfc1e6d2208ff898f
Sumit Garg <sumit.garg@rackspace.com>
2025-08-06 12:52:22 +0530

    Error handling in nato_conversion.py

--- nato_conversion.py
+++ nato_conversion.py
@@ -2,9 +2,26 @@
 
 import pandas
 
+from helpers.colors import color
+
 DATA_FILE = "static/nato_conversion/nato_phonetic_alphabet.csv"
-data = pandas.read_csv(DATA_FILE)
-PHONETIC_DICT = {row.letter: row.code for (index, row) in data.iterrows()}
-USER_WORD = input("Enter the name or word: ").upper()
-CODE_LIST = [PHONETIC_DICT[letter] for letter in USER_WORD]
-print(CODE_LIST)
+
+
+def generate_phonetic(data):
+    """Generate Phonetic"""
+    data = pandas.read_csv(DATA_FILE)
+    phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+    user_word = input("Enter the name or word: ").upper()
+
+    try:
+        code_list = [phonetic_dict[letter] for letter in user_word]
+    except KeyError as e:
+        print(
+            f"{color.RED}Invalid character {e}: Only alphabetic characters are allowed.{color.END}"
+        )
+        generate_phonetic(DATA_FILE)
+    else:
+        print(code_list)
+
+
+generate_phonetic(DATA_FILE)
