ed34af9e8969dde48a55b4a3d7fd12d7119b73bc
Sumit Garg <sumit.garg@rackspace.com>
2025-09-30 09:27:05 +0530

    Please Linters

--- flask_projects/books_library/main.py
+++ flask_projects/books_library/main.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Float
-from sqlalchemy.exc import SQLAlchemyError
+"""Books Library"""
+
 import os
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Float, String
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 DB_FILE = os.path.join(BASEDIR, "books.db")
 
-class Base(DeclarativeBase):
-    pass
+
+class Base(DeclarativeBase):  # pylint: disable=too-few-public-methods
+    """SQLAlchemy Base class"""
+
+    pass  # pylint: disable=unnecessary-pass
+
 
 db = SQLAlchemy(model_class=Base)
 
-class Book(db.Model):
+
+class Book(db.Model):  # pylint: disable=too-few-public-methods
+    """Book DB class"""
+
     __tablename__ = "books"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -22,38 +32,50 @@ class Book(db.Model):
     review: Mapped[float] = mapped_column(Float, nullable=False)
 
     def to_dict(self):
-        return {"id": self.id, "title": self.title, "author": self.author, "review": self.review}
-    
+        """Converts to dict"""
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "review": self.review,
+        }
+
 
 app = Flask(__name__)
 app.secret_key = "SuperSecretKey"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///{}".format(DB_FILE)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_FILE}"
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+
+@app.route("/")
 def home():
+    """Home Page"""
     books = db.session.scalars(db.select(Book).order_by(Book.title))
     all_books = [b.to_dict() for b in books]
     return render_template("index.html", books=all_books)
 
+
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    """Adds new book"""
     if request.method == "POST":
         new_book = Book(
-            title = request.form["title"],
-            author = request.form["author"],
-            review = request.form["rating"]
+            title=request.form["title"],
+            author=request.form["author"],
+            review=request.form["rating"],
         )
         db.session.add(new_book)
         db.session.commit()
         return redirect(url_for("home"))
     return render_template("add.html")
 
+
 @app.route("/edit/<int:book_id>", methods=["GET", "POST"])
 def edit(book_id):
+    """Edits book rating"""
     book = db.get_or_404(Book, book_id)
 
     if request.method == "POST":
@@ -66,11 +88,13 @@ def edit(book_id):
                 return redirect(url_for("home"))
             except ValueError:
                 return "Invalid rating!", 400
-            
+
     return render_template("edit.html", book=book)
+
 
 @app.route("/delete/<int:book_id>", methods=["POST"])
 def delete(book_id):
+    """Delete book from library"""
     book = db.get_or_404(Book, book_id)
 
     try:
@@ -79,10 +103,11 @@ def delete(book_id):
         flash("Book delete successfully!", "success")
     except SQLAlchemyError as e:
         db.session.rollback()
+        print(e)
         flash("Failed to delete book!", "danger")
 
     return redirect(url_for("home"))
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
